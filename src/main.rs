5ca32df4afc95d2f85628d1d41421d01b505f1b0
Ivan Lazarevic <24551171+ivanbgd@users.noreply.github.com>
2024-03-13 21:55:46 +0100

    Have `main()` use `tokio::main` instead of `actix::main` - we don't use Actix anymore

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use stock::logic::main_loop;
 use stock_trading_cli_with_async_streams as stock;
 
-#[actix::main]
+#[tokio::main]
 async fn main() -> Result<(), actix::MailboxError> {
     println!();
 
