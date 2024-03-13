2d26ce795876542e0f4fbe0d10c18eab20a84f3c
Ivan Lazarevic <24551171+ivanbgd@users.noreply.github.com>
2024-03-13 23:19:05 +0100

    Have `main()` return generic `Error` (for now) - we don't use Actix anymore

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
+use std::error::Error;
+
 use stock::logic::main_loop;
 use stock_trading_cli_with_async_streams as stock;
 
 #[tokio::main]
-async fn main() -> Result<(), actix::MailboxError> {
+// async fn main() -> Result<(), actix::MailboxError> {
+// todo change error type?
+async fn main() -> Result<(), Box<dyn Error>> {
     println!();
 
     main_loop().await?;
